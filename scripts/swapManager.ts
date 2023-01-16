9eabe8055c050d6f4c3efcbc783b19a54abb4618
BTC Oracle <carlhamilton1020@gmail.com>
2023-01-16 13:38:00 +1000

    Contract development update - added 3 lines

--- scripts/swapManager.ts
+++ scripts/swapManager.ts
@@ -1,1 +1,14 @@
 import fs from 'fs'
+import path from 'path'
+
+// Define types
+type TranslationKey = string
+type TranslationKeys = Record<TranslationKey, TranslationKey>
+type Tag = 'unused' | 'missing'
+type TaggedKeys = [string[], Tag]
+
+const TRANSLATIONS_FILE = path.resolve(__dirname, '../packages/localization/src/config/translations.json')
+
+/**
+ * Manages the translations.json file by adding missing keys or removing unused ones
+ * @param keysToProcess Object containing keys in format { "key1": "key1", "key2": "key2" }
